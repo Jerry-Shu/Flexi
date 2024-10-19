dcfdf9e6f71ae90748aa07edb4a5aff3d8bf02bc
Albert Xie <albertxie@berkeley.edu>
2024-10-19 12:01:35 -0700

    some history part

--- Flexi_iOS/Flexi_iOS/HistoryPageView.swift
+++ Flexi_iOS/Flexi_iOS/HistoryPageView.swift
@@ -1,15 +1,22 @@
 import SwiftUI
 
 struct HistoryPageView: View {
+    @State private var selectedDate = Date()
+    
     var body: some View {
         NavigationView {
             VStack {
                 Text("History Page")
                     .font(.largeTitle)
                     .padding()
-
+                
+                // Date Selector
+                DatePicker("Select Date", selection: $selectedDate, displayedComponents: .date)
+                    .datePickerStyle(GraphicalDatePickerStyle())
+                    .padding()
+                
                 Spacer()
-
+                
                 // Content specific to the History page can be added here
             }
             .background(Color.white.edgesIgnoringSafeArea(.all))
