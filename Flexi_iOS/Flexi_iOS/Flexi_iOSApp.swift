6fd85a8518b64cee4aa10de9df0a1625a6e80eaf
Albert Xie <albertxie@berkeley.edu>
2024-10-18 22:01:47 -0700

    create iOS app

---
+++ Flexi_iOS/Flexi_iOS/Flexi_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Flexi_iOSApp.swift
+//  Flexi_iOS
+//
+//  Created by Albert Xie on 10/18/24.
+//
+
+import SwiftUI
+
+@main
+struct Flexi_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Flexi_iOS/Flexi_iOSTests/Flexi_iOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  Flexi_iOSTests.swift
+//  Flexi_iOSTests
+//
+//  Created by Albert Xie on 10/18/24.
+//
+
+import Testing
+@testable import Flexi_iOS
+
+struct Flexi_iOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
